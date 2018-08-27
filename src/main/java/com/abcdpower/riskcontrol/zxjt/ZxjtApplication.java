0f788c07864ba76547856523c68877d39bb86795
botaolin <botaolin@beigebigdata.com>
2018-08-27 16:38:02 +0800

    Initial commit

---
+++ src/main/java/com/abcdpower/riskcontrol/zxjt/ZxjtApplication.java
@@ -0,0 +1,12 @@
+package com.abcdpower.riskcontrol.zxjt.zxjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxjtApplication.class, args);
+    }
+}
